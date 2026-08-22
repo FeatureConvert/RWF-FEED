eeef68130091c90f2135adb0533119e366b08538
Robert <rhouston86@icloud.com>
2026-08-22 16:45:34 -0400

    Initial Commit

---
+++ RWF FEED/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RWF FEED
+//
+//  Created by Robert Houston on 8/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RWF FEED/RWF_FEEDApp.swift
@@ -0,0 +1,17 @@
+//
+//  RWF_FEEDApp.swift
+//  RWF FEED
+//
+//  Created by Robert Houston on 8/22/26.
+//
+
+import SwiftUI
+
+@main
+struct RWF_FEEDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
